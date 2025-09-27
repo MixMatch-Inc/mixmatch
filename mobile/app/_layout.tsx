c5c80191ac8b5bd7d7365e1076ee016a76d87ac4
Hakeem <chemicalcommando@gmail.com>
2025-09-27 15:34:18 +0100

    feat: implement authentication context and login screen with form validation

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,9 +1,31 @@
 import { Stack } from 'expo-router';
+import { AuthProvider, useAuth } from '../context/AuthContext';
+import { ActivityIndicator, View } from 'react-native';
 
-export default function RootLayout() {
+const InitialLayout = () => {
+  const { isLoading, token } = useAuth();
+  
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
     </Stack>
+  );
+};
+
+
+export default function RootLayout() {
+  return (
+    <AuthProvider>
+      <InitialLayout />
+    </AuthProvider>
   );
 }
