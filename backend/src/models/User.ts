becce0c4268d7e40cbe842faa21718079c6a4462
Ugochukwu Nebolisa <nebolisaugochukwu@gmail.com>
2025-09-27 21:41:10 +0100

    chore:code cleanup

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -21,7 +21,7 @@ const userSchema = new Schema<IUser>({
     type: String,
     required: [true, 'Password is required'],
     minlength: [6, 'Password must be at least 6 characters long'],
-    select: false, // Do not return password field by default
+    select: false,
   },
 });
 
